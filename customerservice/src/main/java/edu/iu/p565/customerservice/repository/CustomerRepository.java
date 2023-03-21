d54450c840a9b7a5c724262ae1d6c153ba42dbb5
Servane Cloteaux <scloteau@iu.edu>
2023-03-21 16:14:55 -0400

    repo chnage

--- customerservice/src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
+++ customerservice/src/main/java/edu/iu/p565/customerservice/repository/CustomerRepository.java
@@ -3,9 +3,11 @@ package edu.iu.p565.customerservice.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import edu.iu.p565.customerservice.model.Customer;
 
-
+@Repository
 public class CustomerRepository {
     private List<Customer> customers = new ArrayList<>();
 
